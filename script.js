e1ec5edc5b4577512a9e15771ea1d3a79aa0a572
cmedeiros1130 <cmedeiros1130@gmail.com>
2024-10-08 16:29:51 -0400

    completely redone but have fillers

--- script.js
+++ script.js
@@ -1,44 +1,136 @@
-const navBar = document.querySelector(".navbar");
-const navbarOffsetTop = navBar.offsetTop;
-const sections = document.querySelectorAll("section");
-const navBarLinks = document.querySelectorAll(".navbar-link");
-const progress = document.querySelector(".progress-bars-wrapper");
+const smMenuBtn = document.querySelector(".main-header__sm-scr-nav-btn");
+const smMenu = document.querySelector(".main-header__sm-menu");
+const smMenuCloseBtn = document.querySelector(".main-header__sm-menu-close");
 
-const progressBarPercents = [90, 89, 85, 85, 80, 65, 65, 65];
+const smMenuLinks = document.querySelectorAll(".main-header__sm-menu-link");
+const smMenuLink1 = document.querySelector(".main-header__sm-menu-link--1");
+const smMenuLink2 = document.querySelector(".main-header__sm-menu-link--2");
+const smMenuLink3 = document.querySelector(".main-header__sm-menu-link--3");
+const smMenuLink4 = document.querySelector(".main-header__sm-menu-link--4");
 
-window.addEventListener("scroll", () => {
-  mainFn();
+smMenuBtn.addEventListener("click", () => {
+  smMenu.style.transitionDelay = "0s";
+  smMenu.classList.add("main-header__sm-menu--active");
+
+  smMenuLink1.style.transitionDelay = ".5s";
+  smMenuLink1.style.transform = "translateY(0)";
+  smMenuLink1.style.opacity = "1";
+
+  smMenuLink2.style.transitionDelay = ".8s";
+  smMenuLink2.style.transform = "translateY(0)";
+  smMenuLink2.style.opacity = "1";
+
+  smMenuLink3.style.transitionDelay = "1.1s";
+  smMenuLink3.style.transform = "translateY(0)";
+  smMenuLink3.style.opacity = "1";
+
+  smMenuLink4.style.transitionDelay = "1.4s";
+  smMenuLink4.style.transform = "translateY(0)";
+  smMenuLink4.style.opacity = "1";
 });
 
-const mainFn = () => {
-  if (window.pageYOffset >= navbarOffsetTop) {
-    navBar.classList.add("sticky");
-  } else {
-    navBar.classList.remove("sticky");
-  }
+smMenuLinks.forEach((ele) => {
+  ele.addEventListener("click", () => {
+    smMenuLink4.style.transitionDelay = "0s";
+    smMenuLink4.style.transform = "translateY(50px)";
+    smMenuLink4.style.opacity = "0";
 
-  sections.forEach((section, i) => {
-    if (window.pageYOffset >= section.offsetTop - 10) {
-      navBarLinks.forEach((link) => link.classList.remove("change"));
-      navBarLinks.forEach((navBarLink) => {
-        navBarLink.classList.remove("change");
-      });
+    smMenuLink3.style.transitionDelay = ".3s";
+    smMenuLink3.style.transform = "translateY(50px)";
+    smMenuLink3.style.opacity = "0";
 
-      navBarLinks[i].classList.add("change");
-    }
+    smMenuLink2.style.transitionDelay = ".6s";
+    smMenuLink2.style.transform = "translateY(50px)";
+    smMenuLink2.style.opacity = "0";
+
+    smMenuLink1.style.transitionDelay = ".9s";
+    smMenuLink1.style.transform = "translateY(50px)";
+    smMenuLink1.style.opacity = "0";
+
+    smMenu.style.transitionDelay = "1.2s";
+    smMenu.classList.remove("main-header__sm-menu--active");
+
+    setTimeout(() => {
+      document.getElementById(ele.name).scrollIntoView();
+    }, 1300);
   });
+});
 
-  if (window.pageYOffset + window.innerHeight >= progress.offsetTop) {
-    document.querySelectorAll(".progres-percent").forEach((el, i) => {
-      el.style.width = `${progressBarPercents[i]}%`;
-      el.previousElementSibling.firstElementChild.textContent =
-        progressBarPercents[i];
-    });
+smMenuCloseBtn.addEventListener("click", () => {
+  smMenuLink4.style.transitionDelay = "0s";
+  smMenuLink4.style.transform = "translateY(50px)";
+  smMenuLink4.style.opacity = "0";
+
+  smMenuLink3.style.transitionDelay = ".3s";
+  smMenuLink3.style.transform = "translateY(50px)";
+  smMenuLink3.style.opacity = "0";
+
+  smMenuLink2.style.transitionDelay = ".6s";
+  smMenuLink2.style.transform = "translateY(50px)";
+  smMenuLink2.style.opacity = "0";
+
+  smMenuLink1.style.transitionDelay = ".9s";
+  smMenuLink1.style.transform = "translateY(50px)";
+  smMenuLink1.style.opacity = "0";
+
+  smMenu.style.transitionDelay = "1.2s";
+  smMenu.classList.remove("main-header__sm-menu--active");
+});
+
+// ---
+const themeColorSelector = document.querySelector(".themeClrSelector");
+const themeColorSelectorInput = document.querySelector(
+  ".themeClrSelector__input"
+);
+const root = document.documentElement;
+
+const hexToRgb = (hex) => {
+  let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result
+    ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16),
+      }
+    : null;
+};
+
+const eventFire = (el, etype) => {
+  if (el.fireEvent) {
+    el.fireEvent("on" + etype);
+  } else {
+    let evObj = document.createEvent("Events");
+    evObj.initEvent(etype, true, false);
+    el.dispatchEvent(evObj);
   }
 };
 
-mainFn();
+themeColorSelector.addEventListener("click", () => {
+  eventFire(themeColorSelectorInput, "input");
+});
 
-window.addEventListener("resize", () => {
-  window.location.reload;
+const setDynamicColor = (color) => {
+  const { r, g, b } = hexToRgb(`${color}`);
+
+  root.style.setProperty("--themeColor", `${r},${g},${b}`);
+  //localStorage.setItem('color', color)
+};
+
+themeColorSelectorInput.addEventListener("input", (e) => {
+  setDynamicColor(e.target.value);
+});
+
+// if (localStorage.getItem('color')) {
+//   let userSelectedColor = localStorage.getItem('color')
+//   themeColorSelectorInput.value = userSelectedColor
+//   setDynamicColor(userSelectedColor)
+// }
+
+// ---
+const headerLogoConatiner = document.querySelector(
+  ".main-header__logo-container"
+);
+
+headerLogoConatiner.addEventListener("click", () => {
+  location.href = "index.html";
 });
